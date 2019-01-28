17fce0d36211124462d7e25f22b9114dd0486b45
jaume <jmila@leadtech.com>
2019-01-28 14:24:25 +0900

    FIX Packaging errors

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='treetositemap',
-      version='0.1.0b4',
+      version='0.1.0b5',
       description='Command line tool that automates the creation of sitemap '
                   'resources and recursively indexes large amount of filtered '
                   'files and directories.',
       author='Jaume Mila',
       author_email='jaume@westial.com',
-      packages=["treetositemap", "treetositemap.src"],
+      packages=find_packages(),
       entry_points={
           "console_scripts": ['treetositemap = treetositemap.__main__:main']
       },
